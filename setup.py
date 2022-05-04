bd5d62becb6a2be942f4a558c79a2e4f4d0fd732
seokko <100738337+seokko@users.noreply.github.com>
2022-05-04 09:23:14 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='seokcalculator001',
+  version='0.0.1',
+  description='oss-dev seokcalculator001',
+  author='seokjinkang',
+  author_email='2615208@naver.com',
+  url = 'https://github.com/seokko/mycalc',
+  install_requires=['pytest'],
+  long_description = 'oss-dev mycalc',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
